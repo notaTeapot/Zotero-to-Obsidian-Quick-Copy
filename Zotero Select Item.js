70dd7a4a95d2a4e16ecd4a21cdc5ea746514fc59
Vasco Berl <IamreallynotaTeapot@gmail.com>
2023-05-08 12:02:40 +0200

    Add files via upload

---
+++ Zotero Select Item.js
@@ -0,0 +1,26 @@
+{
+"translatorID":"76a79119-3a32-453a-a0a9-c92640e3c93b",
+"translatorType":2,
+"label":"Zotero to Obsidian",
+"creator":"Vasco Berl, Scott Campbell, Avram Lyon",
+"target":"html",
+"minVersion":"2.0",
+"maxVersion":"",
+"priority":200,
+"inRepository":false,
+"lastUpdated":"2022-04-04 11:30:00"
+}
+
+function doExport() {
+	var item;
+	while(item = Zotero.nextItem()) {
+		var author = item.creators[0].lastName;
+        var year = item.date.slice(0,4);
+
+		Zotero.write("[" + author + year + "](" );
+		Zotero.write("zotero://select/items/");
+		var library_id = item.libraryID ? item.libraryID : 0;
+		Zotero.write(library_id+"_"+item.key);
+		Zotero.write(")");
+	}
+}
